736e927ad26e89fbe7639fd9b50be3084bb6c61a
Trystan <145976871+trystancesi@users.noreply.github.com>
2026-01-23 10:46:38 +0100

    Indentation + ajout d'un script powershell pour la compilation multi-cartes + passage de BLEDevice à NimBLE pour le bluetooth + suppression des README

--- embedded/ESP32/ESP32/src/main.cpp
+++ embedded/ESP32/ESP32/src/main.cpp
@@ -1,82 +1,68 @@
-#include<Arduino.h>
-
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLE2902.h>  // Pour les notifications
-#include <BLE2904.h>
+#include <Arduino.h>
+#include <NimBLEDevice.h>
 
 #define BUILTIN_LED 2
 
-#define SERVICE_ENV_UUID BLEUUID((uint16_t)0x181A)
-#define CHAR_TEMP_UUID "f1047d07-53c8-4877-9c5f-29f7161c516d"
+#define SERVICE_ENV_UUID        "181A"
+#define CHAR_TEMP_UUID          "f1047d07-53c8-4877-9c5f-29f7161c516d"
 
-#define SERVICE_AUTOMATION_UUID BLEUUID((uint16_t)0x1815)
-#define CHAR_LED_UUID BLEUUID((uint16_t)0x2A56)
-
-//#define SERVICE_ENV_UUID "dc8d9a41-8f44-4c0b-8e2b-c9493449d2bd"
-//#define CHAR_TEMP_UUID "f1047d07-53c8-4877-9c5f-29f7161c516d"
-
-//#define SERVICE_AUTOMATION_UUID  "ca1e56c8-31b6-4746-ba87-1742270fcf2e"
-//#define CHAR_LED_UUID "ab2cf69d-cbb6-4128-94d4-cc454136f97e"
+#define SERVICE_AUTOMATION_UUID "1815"
+#define CHAR_LED_UUID           "2A56"
 
 #define DEVICE_NAME "EcoGuard_GrpX"
 
-// Variables globales
 float temperature = 20.0;
 bool deviceConnected = false;
 
-BLEServer* pServer = NULL;
-BLEService* pEnvService = NULL;
-BLEService* pAutomationService = NULL;
-BLECharacteristic* pTempCharacteristic = NULL;
-BLECharacteristic* pLEDCharacteristic = NULL;
-BLEAdvertising* pAdvertising = NULL;
-BLE2904* pFormat = NULL;
+NimBLEServer* pServer = nullptr;
+NimBLEService* pEnvService = nullptr;
+NimBLEService* pAutomationService = nullptr;
+NimBLECharacteristic* pTempCharacteristic = nullptr;
+NimBLECharacteristic* pLEDCharacteristic = nullptr;
 
-#define NOTIFY_CHARACTERISTIC
+/* ===================== CALLBACKS ===================== */
 
-class ServerCallbacks : public BLEServerCallbacks 
+class ServerCallbacks : public NimBLEServerCallbacks 
 {
-    void onConnect(BLEServer* pServer) 
-    {
-        deviceConnected = true;
-        Serial.println("Client BLE connecté");
-    }
+  void onConnect(NimBLEServer* pServer) override 
+  {
+    deviceConnected = true;
+    Serial.println("Client BLE connecté");
+  }
 
-    void onDisconnect(BLEServer* pServer) 
-    {
-        deviceConnected = false;
-        Serial.println("Client BLE déconnecté");
-        // Redémarrer l'advertising pour permettre une nouvelle connexion
-        BLEDevice::startAdvertising();
-    }
+  void onDisconnect(NimBLEServer* pServer) override 
+  {
+      deviceConnected = false;
+      Serial.println("Client BLE déconnecté");
+      NimBLEDevice::getAdvertising()->start();
+  }
 };
 
-class LEDCallbacks : public BLECharacteristicCallbacks
+
+class LEDCallbacks : public NimBLECharacteristicCallbacks 
 {
-  void onWrite(BLECharacteristic* pCharacteristic)
+  void onWrite(NimBLECharacteristic* pCharacteristic) override 
   {
     std::string value = pCharacteristic->getValue();
+    if (value.empty()) return;
 
-    uint8_t cmd = (uint8_t)value[0];
+    uint8_t cmd = value[0];
 
-    switch (cmd) 
+    if (cmd == 0x01) 
     {
-      case 0x01:
-        digitalWrite(BUILTIN_LED, HIGH);
-        Serial.println("LED allumée");
-        break;
-
-      case 0x00:
-        digitalWrite(BUILTIN_LED, LOW);
-        Serial.println("LED éteinte");
-        break;
-
-      default:
-        Serial.println("Commande LED inconnue");
+      digitalWrite(BUILTIN_LED, HIGH);
+      Serial.println("LED allumée");
+    } else if (cmd == 0x00) 
+    {
+      digitalWrite(BUILTIN_LED, LOW);
+      Serial.println("LED éteinte");
     }
   }
 };
+
+
+
+/* ===================== SETUP ===================== */
 
 void setup() 
 {
@@ -84,68 +70,69 @@ void setup()
   digitalWrite(BUILTIN_LED, LOW);
 
   Serial.begin(115200);
-  Serial.println("Test BLE init server");
+  Serial.println("Init BLE NimBLE");
 
-  BLEDevice::init(DEVICE_NAME);
+  /* Init NimBLE */
+  NimBLEDevice::init(DEVICE_NAME);
+  delay(100);
+  NimBLEDevice::setPower(ESP_PWR_LVL_P9);   // Puissance max (optionnel)
 
-  pServer = BLEDevice::createServer();
+  pServer = NimBLEDevice::createServer();
   pServer->setCallbacks(new ServerCallbacks());
 
+  /* Services */
   pEnvService = pServer->createService(SERVICE_ENV_UUID);
   pAutomationService = pServer->createService(SERVICE_AUTOMATION_UUID);
 
-  #ifdef NOTIFY_CHARACTERISTIC
-  pTempCharacteristic = pEnvService->createCharacteristic(CHAR_TEMP_UUID,BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
-  pTempCharacteristic->addDescriptor(new BLE2902());
-  #else
-  pTempCharacteristic = pEnvService->createCharacteristic(CHAR_TEMP_UUID, BLECharacteristic::PROPERTY_READ);
-  #endif
 
-  pLEDCharacteristic = pAutomationService->createCharacteristic(CHAR_LED_UUID, BLECharacteristic::PROPERTY_WRITE);
-  pLEDCharacteristic->setCallbacks(new LEDCallbacks());
 
-  pFormat = new BLE2904();
-  pFormat->setFormat(BLE2904::FORMAT_FLOAT32);
+  /* Température */
+  pTempCharacteristic = pEnvService->createCharacteristic(
+    CHAR_TEMP_UUID,
+    NIMBLE_PROPERTY::READ |
+    NIMBLE_PROPERTY::NOTIFY |
+    NIMBLE_PROPERTY::INDICATE
+  );
+
+  // Descriptor 0x2904 (Characteristic Presentation Format)
+  NimBLE2904* pFormat = new NimBLE2904();
+  pFormat->setFormat(NimBLE2904::FORMAT_FLOAT32);
   pFormat->setUnit(0x272F); // °C
   pFormat->setExponent(0);
   pTempCharacteristic->addDescriptor(pFormat);
 
+  /* LED */
+  pLEDCharacteristic = pAutomationService->createCharacteristic(
+    CHAR_LED_UUID,
+    NIMBLE_PROPERTY::WRITE
+  );
+  pLEDCharacteristic->setCallbacks(new LEDCallbacks());
+
   pEnvService->start();
   pAutomationService->start();
 
-  pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_ENV_UUID);
-  pAdvertising->addServiceUUID(SERVICE_AUTOMATION_UUID);
+  /* Advertising */
+  NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->setName(DEVICE_NAME);
   pAdvertising->setScanResponse(true);
-  BLEDevice::startAdvertising();
+  pAdvertising->start();
 
-  Serial.println("Test BLE start advertising");
-
-  Serial.println("Test BLE wait connection");
+  Serial.println("Advertising started");
 }
+
+/* ===================== LOOP ===================== */
 
 void loop() 
 {
   temperature += 0.5;
+  if (temperature > 40.0) temperature = 20.0;
 
-  if (temperature > 40.0) 
+  if (deviceConnected && pTempCharacteristic->getSubscribedCount() > 0) 
   {
-      temperature = 20.0;
-  }
-
-  if (deviceConnected) 
-  {
-    pTempCharacteristic -> setValue(temperature);
-
-    #ifdef NOTIFY_CHARACTERISTIC
+    pTempCharacteristic->setValue(temperature);
     pTempCharacteristic->notify();
-    #endif
-    delay(10);//Pour éviter la congestion du Bluetooth
-
-    String datas(temperature);
     Serial.printf("BLE Notify : %.2f\n", temperature);
-    // TODO: Mettre à jour la température
-    // TODO: Notifier le client
-    delay(2000);
   }
+
+  delay(2000);
 }
